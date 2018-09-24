e4ab240f96dd853339398f9ab19ae0c64ed9d1b7
Teodor Penkov <teodor.penkov@receipt-bank.com>
2018-09-24 22:13:14 +0300

    Remove debug logs

--- src/main/kotlin/menu/Parser.kt
+++ src/main/kotlin/menu/Parser.kt
@@ -15,7 +15,6 @@ abstract class AlmaParser<T> {
 
     fun parseFile(file: String): T {
         tokens.addAll(file.split("\n"))
-        println("tokens $tokens")
         consumeTokens()
         return build()
     }
@@ -60,14 +59,12 @@ abstract class AlmaParser<T> {
 
     private fun consumeCategory() {
         currentCategory = currentToken()
-        println("current category: $currentCategory!!")
         currentTokenIndex++
     }
 
     private fun consumeItemsForCategory(category: String) {
         var currentToken = currentToken()
         while (currentToken != null && !currentToken.startsWith("-")) {
-            println("consuming $category")
             var itemTokens = currentToken.split(" - ")
             var name = itemTokens[0]
             var price = itemTokens[1]
